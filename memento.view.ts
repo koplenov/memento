09c4ff3e4823abe79dc548f32150824a4e8ae351
koplenov <koplenov@gmail.com>
2022-09-07 15:10:27 +0300

    cleanup code

--- memento.view.ts
+++ memento.view.ts
@@ -1,164 +1,116 @@
 namespace $.$$ {
-	const baseMementosDir: string = 'mementos';
-	
+	const baseMementosDir: string = 'mementos'
+	const contentMementoSpec: string = 'content.md'
+	const metaMementoSpec: string = 'meta.memento'
+
 	export class $memento_content extends $.$memento_content {
-		
-		@ $mol_mem
+
+		@$mol_mem
 		pages() {
 			return [
 				this.Menu(),
-				... $mol_maybe( this.Spread() ),
+				...$mol_maybe( this.Spread() ),
 			]
 		}
-		
+
 		Spread() {
 			return this.spreads()[ this.spread() ]
 		}
-		
-		@ $mol_mem
+
+		@$mol_mem
 		spread( next?: string ) {
 			return this.$.$mol_state_arg.value( this.param(), next ) ?? ''
 		}
 
-		@ $mol_mem
+		@$mol_mem
 		links() {
 			return Object.keys( this.spreads() ).map( spread => this.Link( spread ) )
 		}
 
-		auto(){
-			this.$.$mol_lights(true)
-		}
-		
-		@ $mol_mem
-		tauri_funcs(){
-		  return $mol_wire_sync({ ... window.__TAURI__.fs })
-		}
-	  
-		@ $mol_mem
-		tauri(){
-		  const tauri = this.tauri_funcs()
-		  tauri.createDir(baseMementosDir, { dir: this.tauri_funcs().BaseDirectory.App, recursive: true });
-		  return tauri
+		auto() {
+			this.$.$mol_lights( true )
 		}
 
-		@ $mol_mem
-		page_title(id: any) {
+		@$mol_mem
+		tauri_funcs() {
+			return $mol_wire_sync( { ...window.__TAURI__.fs } )
+		}
+
+		@$mol_mem
+		tauri() {
+			const tauri = this.tauri_funcs()
+			tauri.createDir( baseMementosDir, { dir: this.tauri_funcs().BaseDirectory.App, recursive: true } )
+			return tauri
+		}
+
+		@$mol_mem
+		page_title( id: any ) {
 			return id
 		}
 
-		/*
-		// left bar
-		@ $mol_mem
-		hided(val?: any){
-			return val || null
-		}
-
-		@ $mol_mem
-		showBar(val?: any) {
-			this.hided(null)
-		}
-		
-
-		@ $mol_mem
-		hideBar(val?: any) {
-			this.hided(true)
-		}
-		*/
-
-		@ $mol_mem
+		@$mol_mem
 		resets( reset?: null ) {
-    		return Math.random()
+			return Math.random()
 		}
 
 		// pages
-		@ $mol_mem
-		loadInfo(){
-			this.preparate()
+		@$mol_mem
+		loadInfo() {
 			this.resets() // слушаем ресеты
-			let pages = []
-			const entries = this.tauri().readDir(baseMementosDir, { dir: this.tauri().BaseDirectory.App, recursive: true });
-			for (const entry of entries) {
-
-				let employee: MementoInfo = {
-					title: entry.name, 
-					content: "Tom"
-				}
-				pages.push(employee)
-			}
-			return pages as MementoInfo[];
+			return this.tauri().readDir( baseMementosDir, { dir: this.tauri().BaseDirectory.App, recursive: true } )
 		}
 
-		addMemento(content: string){
-			if(content.trim() === ""){
-				alert("Попытка добавить пустую ссылку!")
+		addMemento( id: string ) {
+			if( id.trim() === "" ) {
+				alert( "Попытка добавить пустую ссылку!" )
 				return
 			}
-			this.tauri().createDir(baseMementosDir +'/'+ content, { dir: this.tauri().BaseDirectory.App, recursive: true });
+			const path = baseMementosDir + '/' + id
+			this.tauri().createDir( path, { dir: this.tauri().BaseDirectory.App, recursive: true } )
+			this.tauri().writeTextFile( path + '/' + contentMementoSpec, 'write simple text here', { dir: this.tauri().BaseDirectory.App } )
+			this.tauri().writeTextFile( path + '/' + metaMementoSpec, '', { dir: this.tauri().BaseDirectory.App } )
 			this.resets( null ) // форсируем ресет
-			/// let pages = [...this.$.$mol_state_local.value( "db" ) as MementoInfo[]]
-			/// pages.push({ title: content, content: "Добавьте описание"})
-			/// this.$.$mol_state_local.value( "db", pages )
 		}
 
-		deleteMemento(id: string){
-			let pages = [...this.$.$mol_state_local.value( "db" ) as MementoInfo[]]
-			pages = this.arrayRemove(pages, id)
-			this.$.$mol_state_local.value( "db", pages )
+		arrayRemove( arr: MementoInfo[], value: string ) {
+			return arr.filter( function( ele: MementoInfo ) {
+				return ele.title != value
+			} )
 		}
 
-		arrayRemove(arr: MementoInfo[], value: string) { 
-			return arr.filter(function(ele: MementoInfo){ 
-				return ele.title != value; 
-			});
-		}
-
-		loadPages(){
+		loadPages() {
 			let pages = []
-			for (const iterator of this.loadInfo()) {
+			for( const iterator of this.loadInfo() ) {
 				let page = iterator as MementoInfo
-				pages.push(this.Page(page.title))
+				pages.push( this.Page( page ) )
 			}
 			return pages
-		}
-
-		@ $mol_mem_key
-		updateContent(title: string, val?: any){
-			if ( val !== undefined ){
-				function isNeedMemento(memento: MementoInfo) {
-					return memento.title === title;
-				}
-				let pages = [...this.$.$mol_state_local.value( "db" ) as MementoInfo[]]
-				let index = pages.findIndex(isNeedMemento)
-				pages[index].content = val
-				this.$.$mol_state_local.value( "db", pages )
-				return val as never
-			}
-			return ""
 		}
 
 		spreads() {
-			let pages =  this.loadPages()
-			return pages
-			// sipmle search
+			let pages = this.loadPages()
+			// simple search
 			let search = this.search().query()
-			return pages.filter(function(ele){
-				return ele !== undefined && ele.title().includes(search) 
-			});
+			return pages.filter( function( ele ) {
+				return ele !== undefined && ele.title().includes( search )
+			} )
 		}
 
-		@ $mol_mem_key
-		content(id: any, next?: any) {
-			return this.$.$mol_state_local.value(id, next) ?? ''
+		@$mol_mem_key
+		content( id: any, next?: any ) {
+			const path = baseMementosDir + '/' + id
+			this.tauri().writeTextFile( path + '/' + contentMementoSpec, next, { dir: this.tauri().BaseDirectory.App } )
+			return this.tauri().readTextFile( path + '/' + contentMementoSpec, { dir: this.tauri().BaseDirectory.App } )
+			// or fast cache return this.$.$mol_state_local.value( id, next ) ?? ''
 		}
 
 		addKeyFromSearch() {
-			this.addMemento(this.search().query())
+			this.addMemento( this.search().query() )
 		}
 
-		@ $mol_mem_key
-		removeMementoButton(id: any, next?: any) {
-			this.tauri().removeDir(baseMementosDir +'/'+ id, { dir: this.tauri().BaseDirectory.App });
-			this.deleteMemento(id)
+		@$mol_mem_key
+		removeMementoButton( id: any, next?: any ) {
+			this.tauri().removeDir( baseMementosDir + '/' + id, { dir: this.tauri().BaseDirectory.App, recursive: true } )
 			this.resets( null ) // форсируем ресет
 		}
 	}
