146d8068e5f727011deadf80efa61a8672748dcf
koplenov <koplenov@gmail.com>
2022-10-09 15:34:56 +0300

    added first vk lib

---
+++ lib/vk/vk.ts
@@ -0,0 +1,36 @@
+namespace $ {
+	export class $memento_lib_vk extends $mol_object {
+		constructor(vktoken: string) {
+			super();
+			this.accessToken(vktoken)
+			return this
+		}
+		
+		@$mol_mem
+		apiUrl() {
+			return 'https://api.vk.com/method/'
+		}
+		
+		@$mol_mem
+		version() {
+			return 5.131
+		}
+
+		@$mol_mem
+		accessToken(accessToken?: string) {
+			return accessToken
+		}
+		
+		@$mol_action
+		wall(wallId: string) {
+			const url = `${this.apiUrl()}wall.getById?posts=${wallId}&access_token=${this.accessToken()}&v=${this.version()}`
+			return this.tauri_net_funcs().fetch(url).data.response
+		}
+
+		@$mol_action
+		tauri_net_funcs() {
+			return $mol_wire_sync( { ...window.__TAURI__.http } )
+		}
+	}
+}
+
